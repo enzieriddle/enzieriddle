337226319a7b1e45c102e64c033645cb940b6690
Enzie Riddle <enzieriddle@Enzies-Air.attlocal.net>
2022-10-27 02:14:49 -0500

    Removes unused components from contactFormApp

--- src/contactFormApp/components/Content.js
+++
@@ -1,8 +0,0 @@
-// export default function Content() {
-//     return (
-//         <div className="form-group">
-//             {/* <label for="contentInput">Content</label> */}
-//             <textarea type="content" className="form-control" id="contentInput" name="message" value={toSend.message} onChange={handleChange} aria-describedby="contentHelp" placeholder="How can Enzie help you?" required></textarea>
-//         </div>
-//     );
-// }
--- src/contactFormApp/components/Email.js
+++
@@ -1,9 +0,0 @@
-// export default function Email() {
-//     return (
-//         <div className="form-group col-lg-6">
-//             <label htmlFor="emailInput">Email address</label>
-//             <input type="email" className="form-control" id="emailInput" name="reply_to" value={toSend.reply_to} onChange={handleChange} aria-describedby="emailHelp" required></input>
-//             <small id="emailHelp" className="form-text text-muted">Your email will never be shared with anyone else.</small>
-//         </div>
-//     );
-// }
